f78c902006ea4ae560179ca2cb7d3fa6ed785af3
xiaojun <xiaojun@xiaojundeMBP.bbrouter>
2021-03-07 11:54:52 +0800

    init Ams

---
+++ Ams/src/main/java/com/xiaojun/ams/dao/StudentDao.java
@@ -0,0 +1,8 @@
+package com.xiaojun.ams.dao;
+
+public interface StudentDao {
+     int addUser();
+     int updateUser();
+     int deleteUser();
+     int getUserByName();
+}
---
+++ Ams/src/main/java/com/xiaojun/ams/service/student/StudentService.java
@@ -0,0 +1,8 @@
+package com.xiaojun.ams.service.student;
+
+public interface StudentService {
+    int addUser();
+    int updateUser();
+    int deleteUser();
+    int getUserByName();
+}
